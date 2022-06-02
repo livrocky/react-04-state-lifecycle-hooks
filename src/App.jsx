0e39e4845a99bdec47f060815ff5cf1f81edca0f
rokas <rokasra@gmail.com>
2022-06-02 10:11:52 +0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
+import { useState } from 'react';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
-  // State - react komponento specialus kintamasis nuo kurio gali prisklausyti kaip atsivaizduoja jsx
+  // State - react componento specialus kintamasis nuo kurio gali priklausyti kaip atvsivaizduoja jsx
   console.log('<App /> ran');
+  const [isLoggedInState, setIsLoggedInState] = useState(true);
+  // const isLoggedInState = loggedStateResult[0]
+  // console.log('isLoggedInState ===', isLoggedInState);
+
   let isLoggedIn = true;
 
   setTimeout(() => {
+    console.log('setTimeout ran ====================');
     isLoggedIn = false;
-    console.log('isLoggedIn===', isLoggedIn);
+    // niekada tiesiogiai neprilyginam state jokiai reiksmei
+    // isLoggedInState = false;
+    // setIsLoggedInState(false);
+    // console.log('isLoggedInState ===', isLoggedInState);
+    // console.log('isLoggedIn ===', isLoggedIn);
   }, 3000);
 
   return (
     <div className='App container'>
       <h1>App</h1>
-      <h2>isLoggedIn: {isLoggedIn.toString()}</h2>
-      <h2>{isLoggedIn ? 'is logged' : 'is not logged in'}</h2>
+      <h2>isLoggedInState: {isLoggedInState.toString()}</h2>
+      <h2>{isLoggedInState ? 'is logged' : 'is not logged in'}</h2>
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+// create Counter komponeta
+// atvaizdujam div ir h2
+// h2 reiksme bus state kintamasis (0)
+// kas 2 sekundes norim didinti kintamojo reiksme 1
+
+function Counter() {
+  console.log('<Counter/> ran');
+  const [counterValue, setCouterValue] = useState(0);
+
+  function padidinti() {
+    // jei musu nauja busena priklauso nuo pries tai buvusios
+    // mes turim antaujinimo reiksme paduoti su funkcija
+    setCouterValue((prevState) => {
+      console.log('prevState ===', prevState);
+      return prevState + 1;
+    });
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      // setCouterValue(counterValue + 1);
+      // counterValue += 1;
+      // counterValue = counterValue + 1;
+      padidinti();
+    }, 3000);
+  }, []);
+
+  return (
+    <div>
+      <h2>counter Value {counterValue}</h2>
+      <button onClick={padidinti}>up</button>
+    </div>
+  );
+}
+
+export default Counter;
