e92c5d1d9688c10e2ad024aa3e765112b1d8ced0
rokas <rokasra@gmail.com>
2022-06-02 08:58:16 +0300

    ui neatsinaujina

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
 
 function App() {
+  // State - react komponento specialus kintamasis nuo kurio gali prisklausyti kaip atsivaizduoja jsx
+  console.log('<App /> ran');
+  let isLoggedIn = true;
+
+  setTimeout(() => {
+    isLoggedIn = false;
+    console.log('isLoggedIn===', isLoggedIn);
+  }, 3000);
+
   return (
-    <div className='App'>
+    <div className='App container'>
       <h1>App</h1>
+      <h2>isLoggedIn: {isLoggedIn.toString()}</h2>
+      <h2>{isLoggedIn ? 'is logged' : 'is not logged in'}</h2>
     </div>
   );
 }
